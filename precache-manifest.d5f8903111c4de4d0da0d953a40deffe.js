2609027f384226bc48a921c1bef2c2e660027beb
Taylor <tcuneo5@gmail.com>
2019-01-18 02:25:58 -0500

    Updates

---
+++ precache-manifest.d5f8903111c4de4d0da0d953a40deffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "ca3d6d5cd59c1f024c0a",
+    "url": "/clicky-game/static/js/main.ca3d6d5c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "ca3d6d5cd59c1f024c0a",
+    "url": "/clicky-game/static/css/main.52e9d3ac.chunk.css"
+  },
+  {
+    "revision": "45ed8f78bcb18a0c6077e09f5378710f",
+    "url": "/clicky-game/index.html"
+  }
+];
